1896fa4abbe1fed6c6269c498b48fa7c5a0585ff
Jowita <jowita.lapies@gmail.com>
2020-02-26 10:18:52 +0000

    new features

---
+++ file.js
@@ -0,0 +1,8 @@
+var databse = [{ username: "tom", password: "smith" }];
+var newsFeed = [
+  { username: "Bobby", timeline: "So tired of life" },
+  { username: "Sally", timeline: "Sooo coool" }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
